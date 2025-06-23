001722de0de4dd1ad818ab26aa5df6f4e7cc9246
Barnabas James <barnabasjames27@gmail.com>
2025-06-23 23:10:29 +0100

    Just started made the icon replacable

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 const form = document.getElementById("form");
+const droparea = document.getElementById("droparea");
+const fileInput = document.getElementById("avatar");
+const avatar = document.getElementById("avatar-icon");
+
+fileInput.addEventListener("change", updateAvatar);
+
+function updateAvatar() {
+  const file = fileInput.files[0];
+  let imgUrl = URL.createObjectURL(file);
+  avatar.src = imgUrl;
+}
